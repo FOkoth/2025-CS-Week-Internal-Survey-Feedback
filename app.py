d17dcfb0693709285f0b6a346f93177be01acbae
FOkoth <fokoth027@gmail.com>
2025-10-07 18:43:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("📊 HELB Service Delivery Analysis")
 # Option 1: Load file directly from GitHub (static)
 @st.cache_data
 def load_data():
-    url = "https://raw.githubusercontent.com/FOkoth/helb-service-delivery-analysis/main/data/Issues%20Influencing%20Excellent%20Service%20Delivery(1-45).xlsx"
+    url = "Issues Influencing Excellent Service Delivery(1-45).xlsx"
     return pd.read_excel(url)
 
 try:
